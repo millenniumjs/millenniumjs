207a4f8de3df7d20e8edeeeaea00af43282af121
afonsopacifer <afonsopolis@gmail.com>
2017-08-04 16:26:26 -0300

    Make vDoms Representations from an array of vDoms feature added

--- src/vdom/vdom.js
+++ src/vdom/vdom.js
@@ -1,48 +1,45 @@
 /**
- * Generates a virtual DOM representation as a plane Object.
+ * Generates a virtual DOM representation as a plane Object
  * @module src/vdom
  *
- * @param {(string|function)} type - Element type.
- * @param {object} props - All Props and values.
- * @param {...(string|vdom)} children -  Child elements (String or vdom)
+ * @param {(string|function)} type - Element type
+ * @param {object} props - All Props and values
+ * @param {...(string|vdom|array)} children -  Child elements
  *
  * @returns {object} Virtual DOM representation as a plane object
  *
- * @example
- * import vdom from './vdom';
- *
- * // Text children
- * vdom(
- *  'h1',
- *  {className: 'foo'},
- *  'text'
- * );
- *
- *
- * // Vdom children (Recursion)
- * vdom(
- *  'div',
- *  {id: 'foo'},
- *  vdom('h1', null, 'text')
- * );
- *
- * // Multiple children
- * vdom(
- *  'form',
- *  null,
- *  'text',
- *  vdom('label', null, 'text'),
- *  vdom('input', {className: 'foo', type: 'text'})
- *);
  */
 
 export default (type, props, ...children) => {
 
-  // Check if type is a function provided by JSX
+  // Height Level JSX component / Function component
+  // --------------------------
   if(typeof type !== 'string') {
-    // Run the function with parameters for render internal nodes
+
+    // Run the function with parameters for make internal virtual nodes
     return type(props);
+
   }
+
+  // vNodes array inner children array
+  // --------------------------
+  children.forEach((vNode, index) => {
+
+    if(Array.isArray(vNode)) {
+      const listOfvNodes = vNode;
+
+      // Remove listOfvNodes from children array
+      children.splice(index, 1);
+
+      listOfvNodes.forEach((internalvNode, internalIndex) => {
+        const nextPosition = index + internalIndex;
+
+        // Add internalvNode to children array
+        children.splice(nextPosition, 0, internalvNode);
+      });
+
+    }
+  });
 
   return {type, props: props || {}, children};
 
